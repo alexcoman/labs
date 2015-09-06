a669951d7ac2599dbad796a4e973d02b32b1f379
Alexandru Coman <acoman@cloudbasesolutions.com>
2015-09-06 11:53:08 +0300

    Adaugă problema paranteze

---
+++ exercitii/paranteze/paranteze.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# *-* coding: UTF-8 *-*
+
+"""Tuxy scrie în fiecare zi foarte multe formule matematice.
+
+Pentru că formulele sunt din ce în ce mai complicate trebuie
+să folosească o serie de paranteze și a descoperit că cea
+mai frecventă problemă a lui este că nu toate parantezele
+sunt folosite cum trebuie.
+
+Pentru acest lucru a apelat la ajutorul tău.
+
+Câteva exemple:
+    - []        este bine
+    - []()      este bine
+    - [()()]    este bine
+    - ][        nu este bine
+    - (][][)    nu este bine
+    - [)]()[(]  nu este bine
+"""
+
+
+def este_corect(expresie):
+    """Verifică dacă toate parantezele sunt folosite corespunzător."""
+    pass
+
+
+if __name__ == "__main__":
+    assert este_corect("[()[]]"), "Probleme la expresia 1"
+    assert este_corect("()()[][]"), "Probleme la expresia 2"
+    assert este_corect("([([])])"), "Probleme la expresia 3"
+    assert not este_corect("[)()()()"), "Probleme la expresia 4"
+    assert not este_corect("][[()][]"), "Probleme la expresia 5"
+    assert not este_corect("([()]))"), "Probleme la expresia 6"
+    assert not este_corect("([)]"), "Probleme la expresia 7"
