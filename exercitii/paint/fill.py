7095db539865d65abce61785c8848461d8aaedf2
Alexandru Coman <acoman@cloudbasesolutions.com>
2015-09-07 11:53:08 +0300

    Adaugă problema fill

---
+++ exercitii/paint/fill.py
@@ -0,0 +1,48 @@
+"""Tuxy dorește să împlementeze un nou paint.
+
+În timpul dezvoltării proiectului s-a izbit de o problemă
+pe care nu o poate rezolva singur și a apelat la ajutorul tău.
+
+El dorește să adauge o unealtă care să permită umplerea unei
+forme închise.
+
+Exemplu:
+
+Pornim de la imaginea inițială reprezentată mai jos, trebuie să
+umplem formele în care se află "x":
+
+  |-----*------|          |******------|         |************|
+  |--x--*------|          |******------|         |************|
+  |******------|  ----->  |******------|  -----> |************|
+  |-----******-|          |-----******-|         |-----*******|
+  |-----*---*--|          |-----*---*--|         |-----*---***|
+  |-----*---*-x|          |-----*---*--|         |-----*---***|
+
+"""
+
+
+def umple(imagine, punct):
+    """Funcția primește reprezentarea imaginii și coordonatele unui
+    punct.
+
+    În cazul în care punctul se află într-o formă închisă trebuie să
+    umple forma respectivă cu caracterul "*"
+    """
+    pass
+
+
+def main():
+    imaginea = [
+        ["-", "-", "-", "-", "-", "*", "-", "-", "-", "-", "-", "-"],
+        ["-", "-", "-", "-", "-", "*", "-", "-", "-", "-", "-", "-"],
+        ["-", "-", "-", "-", "-", "*", "-", "-", "-", "-", "-", "-"],
+        ["*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "-"],
+        ["-", "-", "-", "-", "-", "*", "-", "*", "-", "-", "*", "-"],
+        ["-", "-", "-", "-", "-", "*", "-", "*", "-", "-", "*", "-"],
+    ]
+    umple(imaginea, (1, 3))
+    umple(imaginea, (5, 11))
+
+
+if __name__ == "__main__":
+    main()
