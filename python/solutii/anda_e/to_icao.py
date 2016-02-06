7bbe7daef0d95b690ddf2671dbb2457f7569193d
andaElena <ungureanu.anda.elena@gmail.com>
2016-02-06 11:58:03 +0200

    Solutie to_icao

---
+++ python/solutii/anda_e/to_icao.py
@@ -0,0 +1,50 @@
+﻿#!/usr/bin/env python
+# *-* coding: UTF-8 *-*
+
+"""
+Organizaţia Internaţională a Aviaţiei Civile propune un alfabet în care
+fiecărei litere îi este asignat un cuvânt pentru a evita problemele în
+înțelegerea mesajelor critice.
+
+Pentru a se păstra un istoric al conversațiilor s-a decis transcrierea lor
+conform următoarelor reguli:
+    - fiecare cuvânt este scris pe o singură linie
+    - literele din alfabet sunt separate de o virgulă
+
+Următoarea sarcină ți-a fost asignată:
+    Scrie un program care să primească un fișier ce conține mesajul
+    ce trebuie transmis și generează un fișier numit mesaj.icao ce
+    va conține mesajul scris folosin alfabetul ICAO.
+
+Mai jos găsiți un dicționar ce conține o versiune a alfabetului ICAO:
+"""
+
+from __future__ import print_function
+ICAO = {
+    'a': 'alfa', 'b': 'bravo', 'c': 'charlie', 'd': 'delta', 'e': 'echo',
+    'f': 'foxtrot', 'g': 'golf', 'h': 'hotel', 'i': 'india', 'j': 'juliett',
+    'k': 'kilo', 'l': 'lima', 'm': 'mike', 'n': 'november', 'o': 'oscar',
+    'p': 'papa', 'q': 'quebec', 'r': 'romeo', 's': 'sierra', 't': 'tango',
+    'u': 'uniform', 'v': 'victor', 'w': 'whiskey', 'x': 'x-ray', 'y': 'yankee',
+    'z': 'zulu'
+}
+
+
+def icao(mesaj):
+    """Functia va primi calea mesajul ce trebuie transmis ti
+    va genera un fisier numit mesaj.icao_intrare ce va contine
+    mesajul scris folosind alfabetul ICAO.
+    """
+    fisier = open("mesaj.icao_intrare", "w+")
+    for cuvant in mesaj.split():
+        for litera in cuvant:
+            for key, value in ICAO.items():
+                if litera == key:
+                    fisier.write(value)
+                    fisier.writelines(" ")
+        fisier.write("\n")
+    fisier.close()
+
+
+if __name__ == "__main__":
+    icao("Mesajul ce trebuie transmis")
