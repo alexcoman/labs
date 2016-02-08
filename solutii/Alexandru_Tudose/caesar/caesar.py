72dbef63250d7341b6a10fd30521c3c42cd16c74
Alexandru Tudose <alexandrutudose@outlook.com>
2016-02-08 20:10:39 +0200

    Solved caesar

---
+++ solutii/Alexandru_Tudose/caesar/caesar.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# *-* coding: UTF-8 *-*
+
+"""Împăratul a primit serie de mesaje importante pe care este
+important să le descifreze cât mai repede.
+
+Din păcate mesagerul nu a apucat să îi spună împăratul care au fost
+cheile alese pentru fiecare mesaj și tu ai fost ales să descifrezi
+misterul.
+
+Informații:
+    În criptografie, cifrul lui Caesar este o metodă simplă de a cripta
+un mesaj prin înlocuirea fiecărei litere cu litera de pe poziția aflată
+la un n pași de ea în alfabet (unde este n este un număr întreg cunoscut
+"""
+
+
+def decripteaza(mesaj):
+    """Funcția va primi un mesaj criptat folosind cifrul lui Caesar și
+    va încearca să îl decripteze.
+    """
+    cnt = 0
+    lst = []
+    for char in mesaj:
+        if char.isalpha():
+            char = chr(ord('a') + (ord(char) - ord(mesaj[0])) % 26)
+            if cnt > 3 and mesaj[cnt-2] in '.!?' >= 0 or cnt == 0:
+                char = char.upper()
+        lst.append(char)
+        cnt += 1
+    print "%s\n" % "".join(lst)
+
+
+def main():
+    """ Main function.
+    """
+    try:
+        fisier = open("mesaje.secret", "r")
+        mesaje = fisier.read()
+        fisier.close()
+    except IOError:
+        print "Nu am putut obține mesajele."
+        return
+    for mesaj in mesaje.splitlines():
+        decripteaza(mesaj)
+
+if __name__ == "__main__":
+    main()
