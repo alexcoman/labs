b84015becaa5dc18dfc51adafb67149ae94518b1
alexandruantochi <alexandru.antochi@gmail.com>
2016-02-08 14:32:46 +0200

    Added solution for From_ICAO

---
+++ python/solutii/Alexandru Antochi/from_icao.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# *-* coding: UTF-8 *-*
+
+"""
+Organizaţia Internaţională a Aviaţiei Civile propune un alfabet în care
+fiecărei litere îi este asignat un cuvânt pentru a evita problemele în
+înțelegerea mesajelor critice.
+
+Pentru a se păstra un istoric al conversațiilor s-a decis transcrierea lor
+conform următoarelor reguli:
+    - fiecare cuvânt este scris pe o singură linie
+    - literele din alfabet sunt separate de o virgulă
+
+Următoarea sarcină ți-a fost asignată:
+    Scrie un program care să primească un fișier ce conține mesajul
+    brut (scris folosind alfabetul ICAO) și generează un fișier
+    numit icao_intrare ce va conține mesajul inițial.
+
+Mai jos găsiți un dicționar ce conține o versiune a alfabetului ICAO:
+"""
+
+
+ICAO = {
+    'a': 'alfa', 'b': 'bravo', 'c': 'charlie', 'd': 'delta', 'e': 'echo',
+    'f': 'foxtrot', 'g': 'golf', 'h': 'hotel', 'i': 'india', 'j': 'juliett',
+    'k': 'kilo', 'l': 'lima', 'm': 'mike', 'n': 'november', 'o': 'oscar',
+    'p': 'papa', 'q': 'quebec', 'r': 'romeo', 's': 'sierra', 't': 'tango',
+    'u': 'uniform', 'v': 'victor', 'w': 'whiskey', 'x': 'x-ray', 'y': 'yankee',
+    'z': 'zulu'
+}
+
+ICAO_R = dict ( (v,k) for k, v in ICAO.items() )
+
+
+def din_icao(nume_fisier):
+    """Funcția va primi calea către fișierul ce conține mesajul brut și
+    va genera un fișier numit icao_intrare ce va conține mesajul inițial.
+    """
+
+    intrare=open(nume_fisier,'r')
+    iesire=open('icao_intrare.icao','w')
+    mesaj=intrare.read()
+    for reader in mesaj.splitlines():
+        iesire.write(' ')
+        for i in range(0,len(reader.split())):
+            iesire.write(ICAO_R[reader.split()[i]])
+    intrare.close()
+    iesire.close()
+    pass
+
+
+
+if __name__ == "__main__":
+    din_icao("mesaj.icao")
