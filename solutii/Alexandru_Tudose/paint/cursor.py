c231aaaaed482a63480632c42e986ea949f114fd
Alexandru Tudose <alexandrutudose@outlook.com>
2016-02-08 20:15:02 +0200

    Solved paint

---
+++ solutii/Alexandru_Tudose/paint/cursor.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# *-* coding: UTF-8 *-*
+
+"""Tuxy dorește să împlementeze un nou paint pentru consolă.
+
+În timpul dezvoltării proiectului s-a izbit de o problemă
+pe care nu o poate rezolva singur și a apelat la ajutorul tău.
+
+Aplicația ține un istoric al tuturor mișcărilor pe care le-a
+făcut utlizatorul în fișierul istoric.tuxy
+
+Exemplu de istoric.tuxy:
+
+    STÂNGA 2
+    JOS 2
+    DREAPTA 5
+
+Fișierul de mai sus ne spune că utilizatorul a mutat cursorul
+2 căsuțe la stânga după care 2 căsuțe in jos iar ultima acțiune
+a fost să poziționeze cursorul cu 5 căsuțe în dreapta față de
+ultima poziție.
+
+El dorește un utilitar care să îi spună care este distanța dintre
+punctul de origine (0, 0) și poziția curentă a cursorului.
+"""
+
+
+def distanta():
+    """Funcția citește conținutul fișierului istoric.tuxy și
+    calculează distanța dintre punctul de origine și poziția
+    curentă a cursorului.
+    """
+    ccx, ccy = 0, 0
+    with open("istoric.tuxy", "r") as fin:
+        for mesaj in fin:
+            alfa = mesaj.split()
+            beta = alfa[0]
+            if beta[:2] == 'SU':
+                ccx += int(alfa[1])
+            if beta[0] == 'D':
+                ccy += int(alfa[1])
+            if beta[0] == 'J':
+                ccx -= int(alfa[1])
+            if beta[:2] == 'ST':
+                ccy -= int(alfa[1])
+    print (ccx * ccx + ccy * ccy) ** 0.5
+
+if __name__ == "__main__":
+    distanta()
---
+++ solutii/Alexandru_Tudose/paint/fill.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# *-* coding: UTF-8 *-*
+
+"""Tuxy dorește să împlementeze un nou paint pentru consolă.
+
+În timpul dezvoltării proiectului s-a izbit de o problemă
+pe care nu o poate rezolva singur și a apelat la ajutorul tău.
+
+El dorește să adauge o unealtă care să permită umplerea unei
+forme închise.
+
+Exemplu:
+
+Pornim de la imaginea inițială reprezentată mai jos, trebuie să
+umplem formele în care se află "x":
+
+  |-----*------|          |******------|         |************|
+  |--x--*------|          |******------|         |************|
+  |******------|  ----->  |******------|  -----> |************|
+  |-----******-|          |-----******-|         |-----*******|
+  |-----*---*--|          |-----*---*--|         |-----*---***|
+  |-----*---*-x|          |-----*---*--|         |-----*---***|
+
+"""
+
+
+def inrange(imagine, punct):
+    """Punctul apartine imaginii
+    """
+    ccx, ccy = punct
+    length = len(imagine[0])
+    width = len(imagine)
+    if ccx < width and ccx > -1 and ccy < length and ccy > -1:
+        return 1
+    return 0
+
+
+def umple(img, punct):
+    """Funcția primește reprezentarea imaginii și coordonatele unui
+    punct.
+
+    În cazul în care punctul se află într-o formă închisă trebuie să
+    umple forma respectivă cu caracterul "*"
+    """
+    import Queue
+    que = Queue.Queue()
+    que.put(punct)
+    img[punct[0]][punct[1]] = '*'
+    while que.qsize():
+        point = que.get()
+        sus = (point[0] - 1, point[1])
+        jos = (point[0] + 1, point[1])
+        stanga = (point[0], point[1] - 1)
+        dreapta = (point[0], point[1] + 1)
+        if inrange(img, sus) and img[sus[0]][sus[1]] == '-':
+            que.put(sus)
+            img[sus[0]][sus[1]] = '*'
+        if inrange(img, jos) and img[jos[0]][jos[1]] == '-':
+            que.put(jos)
+            img[jos[0]][jos[1]] = '*'
+        if inrange(img, stanga) and img[stanga[0]][stanga[1]] == '-':
+            que.put(stanga)
+            img[stanga[0]][stanga[1]] = '*'
+        if inrange(img, dreapta) and img[dreapta[0]][dreapta[1]] == '-':
+            que.put(dreapta)
+            img[dreapta[0]][dreapta[1]] = '*'
+    print img
+
+
+def main():
+    """Main definition
+    """
+    imaginea = [
+        ["-", "-", "-", "-", "-", "*", "-", "-", "-", "-", "-", "-"],
+        ["-", "-", "-", "-", "-", "*", "-", "-", "-", "-", "-", "-"],
+        ["-", "-", "-", "-", "-", "*", "-", "-", "-", "-", "-", "-"],
+        ["*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "-"],
+        ["-", "-", "-", "-", "-", "*", "-", "*", "-", "-", "*", "-"],
+        ["-", "-", "-", "-", "-", "*", "-", "*", "-", "-", "*", "-"],
+    ]
+    umple(imaginea, (1, 3))
+    umple(imaginea, (1, 10))
+
+if __name__ == "__main__":
+    main()
