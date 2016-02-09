a320d905232d1c2a3eedfade7f107e1aeb4574e8
andaElena <ungureanu.anda.elena@gmail.com>
2016-02-09 15:28:36 +0200

    Solutie parcurgere directoare

---
+++ python/solutii/anda_e/parcurgere_directoare.py
@@ -0,0 +1,29 @@
+﻿
+#!/usr/bin/env python
+# *-* coding: UTF-8 *-*
+"""
+    Programul afiseaza toate fisierele din path-ul
+dat care contin litera 'a' in nume.
+"""
+from __future__ import print_function
+import os
+import sys
+
+
+def parcurgere(fila):
+    """
+    Pentru fiecare fisier, se verifica daca numele
+    contine litera 'a', in caz afirmativ, se afiseaza numele,
+    altfel, daca este director se apeleaza recursiv functia,
+    daca este fisier se trece la urmatorul fisier.
+    """
+    for item in os.listdir(fila):
+        item = os.path.join(fila, item)
+        if os.path.isfile(item) and ('a' in item):
+            print(item)
+        if os.path.isdir(item):
+            parcurgere(item)
+
+
+if __name__ == "__main__":
+    parcurgere(sys.argv[1])
