45a3aa644543cf5ca732d975affef29b059e2aff
Alexandru Coman <acoman@cloudbasesolutions.com>
2016-03-11 11:46:11 +0200

    Adaugă exercițiul Tuxy Bot

---
+++ php/exercitii/tuxy-bot/index.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="ro">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>Tuxy Bot</title>
+
+    <meta name="author" content="" />
+    <meta name="description" content="" />   
+    <meta name="viewport" content="width=device-width, initial-scale=1">    
+</head>
+
+<body>
+    <header>
+        <h1>Tuxy Bot</h1>
+    </header>
+
+    <section id="continut">
+        <?php include_once('_include/_worker.php'); ?>
+    </section>
+
+    <section id="chat">
+        <form action="#" method="post">
+            Command: <input type="text" name="command">
+            <input type="submit"> 
+        </form>
+    </section>
+
+    <footer>
+        <h3>Toate drepturile rezervate. Tuxy Pinguinesc</h3>
+    </footer>
+</body>
+</html>
