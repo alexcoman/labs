f2061a31a9b0ac480f76db3aad1a9d8b8d4600f2
Costin Galan <cgalan@cloudbasesolutions.com>
2016-04-10 13:50:42 +0300

    This commit fixes issue #142

--- bash/exercitii/man.sh
+++ bash/exercitii/man.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# Scrieti un script pentru cauta informatii in manualul unei comenzi
+# Scrieti un script pentru a cauta informatii in manualul unei comenzi
 # ./man.sh nume_script informatie
 echo "Nu ma executa"
