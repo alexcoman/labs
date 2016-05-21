25a285fdd37defc79718da040d3f704bc2698f02
Alexandru Coman <acoman@cloudbasesolutions.com>
2016-05-21 11:21:56 +0300

    Actualizează exercițiul tuxy-init

--- python/exercitii/tuxy-init/init.py
+++ python/exercitii/tuxy-init/init.py
@@ -1,23 +1,7 @@
 #!/usr/bin/env python
 # *-* coding: UTF-8 *-*
+"""Aplicația tuxy-init."""
 
-"""
-Tuxy își dorește un sistem care să automatizeze instalării unui proiect
-pe infrastructura din `TuxyLand`.
-
-Acest proiect de automatizare va primi un fișier de configurare ce va
-conține toate instrucțiunile necesare pentru pregătirea mediului ideal
-în care va rula `TuxyApp`.
-
-Un exemplu de fișier de configurare ar putea fi: tuxy.config din
-directorul python/date_intrare.
-
-Trebuie să dezvoltați o aplicație care să primească un fișier de
-configurare și să rezolve toate sarcinile precizate în acesta.
-
-La sfârșit va trebui să ofere un fișier build.log care să descrie
-toate lucrurile care s-au întâmplat.
-"""
 # pylint: disable=import-error
 
 from __future__ import print_function
