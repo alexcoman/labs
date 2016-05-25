2581b6088064735806d51c6e390da78691bdda66
danieltoncu <daniel.toncu@info.uaic.ro>
2016-05-25 12:36:39 +0300

    Solutia problemei tree.

---
+++ python/solutii/daniel_toncu/tree/tree.py
@@ -0,0 +1,32 @@
+import sys
+import os
+import os.path
+
+indentation_dim = '   '
+
+def print_tree(dirpath, indentation):
+
+	"""Parcurge directorul ce se gaseste la path-ul dirpath si afiseaza numele tuturor instantelor din el. Toate directoarele ce se gasesc in directorul curent, sunt parcurse recursiv."""
+	"""Note: Fisierele sunt precedate de [F], Directoarele sunt precedate de [D], si intrarile necunoscute sunt precedate de [U]."""
+
+	new_indentation = indentation + indentation_dim
+	for filename in os.listdir(dirpath):
+		filepath = os.path.join(dirpath, filename)
+		if os.path.isfile(filepath):
+			print new_indentation + '[F] ' + filename
+		elif os.path.isdir(filepath):
+			print new_indentation + '[D] ' + filename
+			print_tree(filepath, new_indentation)
+		else:
+			print new_indentation + '[U] ' + filename
+
+if __name__ == "__main__":
+	print
+	if len(sys.argv) != 2:
+		print "Prea putine/multe argumente!"
+		print " Usage: python {} <dirpath>".format(sys.argv[0])
+	elif not os.path.isdir(os.path.join(sys.argv[1])):
+		print "{} nu este un director!".format(sys.argv[1])
+	else:
+		print " {}'s Tree:\n".format(sys.argv[1])
+		print_tree(sys.argv[1], ' ')
