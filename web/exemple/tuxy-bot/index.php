fb74a47719f8911c1de544b55af4495f26acbfd7
Alexandru Coman <acoman@cloudbasesolutions.com>
2017-03-26 18:20:29 +0300

    Adaugă o posibilă soluție pentru problema „tuxy-bot”
    Atenție soluția curentă este doar un exemplu. Aceasta
    poate conține secvențe ineficiente, vulnerabile la
    diverse atacuri etc.
    
    Soluția curentă este propusă pentru a servi ca subiect
    de discuții pentru laboratorul următor.

--- web/exemple/tuxy-bot/index.php
+++ web/exemple/tuxy-bot/index.php
@@ -1,34 +1,43 @@
+<?php
+    define("FLAG_OK", true);
+    require('_include/_bot.php');
+?>
+
 <!doctype html>
 <html lang="ro">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Tuxy Bot</title>
 
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <title>Tuxy Bot</title>
+        <meta name="author" content="Tuxy Pinguinescu" />
+        <meta name="description" content="" />
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
 
-    <meta name="author" content="" />
-    <meta name="description" content="" />   
-    <meta name="viewport" content="width=device-width, initial-scale=1">    
-</head>
+    <body>
+        <header>
+            <h1>Tuxy Bot</h1>
+        </header>
 
-<body>
-    <header>
-        <h1>Tuxy Bot</h1>
-    </header>
+        <section id="continut">
+            <?php
+                # FIXME(tuxy): Nu pare o idee buna.
+                foreach($_SESSION["istoric"] as $comanda) {
+                    echo "$comanda <br>";
+                }
+            ?>
+        </section>
 
-    <section id="continut">
-        <?php include_once('_include/_worker.php'); ?>
-    </section>
+        <section id="chat">
+            <form action="#" method="post">
+                Comanda: <input type="text" name="command">
+                <input type="submit">
+            </form>
+        </section>
 
-    <section id="chat">
-        <form action="#" method="post">
-            Command: <input type="text" name="command">
-            <input type="submit"> 
-        </form>
-    </section>
-
-    <footer>
-        <h3>Toate drepturile rezervate. Tuxy Pinguinesc</h3>
-    </footer>
-</body>
+        <footer>
+            <h3>Toate drepturile rezervate. Tuxy Pinguinescu</h3>
+        </footer>
+    </body>
 </html>
